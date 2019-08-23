1104e7e8a935e5a9fca54ad5109152d6ea597643
Madhur Kapoor <madhur.kapoor@gmail.com>
2019-08-23 04:40:14 +0000

    Corrected issue with Update and Delete, added bucket policy for Images bucket

--- course-04/project/c4-final-project-starter-code/backend/src/datalayer/toDoDataAccess.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/datalayer/toDoDataAccess.ts
@@ -47,28 +47,52 @@ export class TodoDataAccess {
     }
 
     async deleteTodo(todoId: string) {
-        this.docClient.delete({
+
+        var params = {
             TableName: this.todoTable,
             Key: {
-                "todoId":todoId
+              todoId: todoId
             }
+          }
+          console.log("Deleting item with key", todoId)
+          await this.docClient.delete(params, function (err, data) {
+          if (err) {
+            console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+          } else {
+              console.log("Delete succeeded:", JSON.stringify(data, null, 2));
+          }
+          }).promise()
 
-        })
     }
 
     async updateTodo(todoId: string, todoUpdate: TodoUpdate) {
-        this.docClient.update({
+
+        var params = {
             TableName: this.todoTable,
             Key: {
-                'todoId' : todoId
+              todoId: todoId
             },
-            UpdateExpression: 'set name = :n, duedate = :d, done = :e',
+            UpdateExpression: "SET #n1 = :n, #d1 = :d, #do1 = :e",
             ExpressionAttributeValues: {
                 ':n': todoUpdate.name,
                 ':d': todoUpdate.dueDate,
                 ':e': todoUpdate.done
+            },
+            ExpressionAttributeNames: {
+              "#n1": "name",
+              "#d1": "dueDate",
+              "#do1": "done"
             }
-        })
+            
+          }
+          
+          await this.docClient.update(params, function(err, data) {
+            if (err) {
+                console.error("Unable to update item. Error :", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Update succeeded:", JSON.stringify(data, null, 2));
+            }
+          }).promise()
 
     }
 
